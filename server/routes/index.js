70a3056d20e4e957e7dfcbd88259a04bc03cfaac
“[Kram <[kramarenko905@gmail.com]>
2018-09-03 20:52:47 +0300

    WEB-012 Added MongoDB requests.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,183 +1,255 @@
 
 var express = require('express');
 var router = express.Router();
+var mongodb = require('mongodb');
 
-router.get('/api/articles/fetchCategories', function(req, res, next) {
+const dbUrl = 'mongodb://localhost:21027/articles_db';
 
-  const testData = [{ id: 1, title: "one", description: "Some description...(1)" }, { id: 2, title: "two", description: "Some description...(2)" }];
-	res.json(testData);
-});
-
-router.get('/api/articles/fetchArticles/:id', function(req, res, next) {
-  
-  const testData =[
-    { id: 1, title: "ahahah", addedBy: "John", abstract: "..some text 1" },
-    { id: 2, title: "hihihi", addedBy: "David", abstract: "..some text 2" }];
-  
-  res.json(testData);
-});
-
-router.get('/api/articles/fetchArticle/:id', function(req, res, next) {
-  
-  const testData = { id: 1, title: "ahahah", body: "asdfffffffffffgbczmx xzcxc hjdksfsd sd" };     
-  res.json(testData);
-});
-
-router.post('/api/articles/saveComment', function(req, res){
-
-  const data = req.body;
-
-  let errors = {};
-  
-  if(data.name === '')
-      errors.name = "Name can't be empty";
-  if(data.email === '')
-      errors.email = "Email can't be empty";
-  if(data.body === '')
-      errors.body = "Body can't be empty";
-
-  const isValid = Object.keys(errors).length === 0; 
-  if(isValid){
-      const { name, email, body, articleId } = req.body;
-      //Save to db here...
-      //Then successful response:
-      res.json({ comment:{ name, email, body, articleId } });
-  } else {
-      res.status(404).json({ errors });
-  }
-});
-
-router.delete('/api/articles/deleteCategory/:id', function(req, res) {
-  
-  res.json({ id : req.params.id });  
-});
-
-router.delete('/api/articles/deleteArticle/:id', function(req, res) {
-  
-  res.json({ id : req.params.id });  
-});
-  
-router.put('/api/articles/updateCategory/:id', function(req, res){
-  
-  const id = req.params.id;
-  const data = req.body;
-
-  let errors = {};
-  
-  if(data.title === '')
-      errors.title = "Title can't be empty";
-
-  if(data.importance === '')
-      errors.importance = "Importance can't be empty";
-
-  if(data.description === '')
-      errors.description = "Description can't be empty";
-
-  const isValid = Object.keys(errors).length === 0; 
-  if(isValid){
-      const { title, importance, description } = req.body;
-      //Save to db here...
-      //Then successful response:
-      res.json({ category : req.body });
-  } else {
-      res.status(404).json({ errors });
-  }
-});
-
-router.post('/api/articles/saveCategory', function(req, res){
+mongodb.MongoClient.connect(dbUrl, function(err, db){
     
-  const data = req.body;
-  let errors = {};
-  
-  if(data.title === '')
-      errors.title = "Title can't be empty";
-
-  if(data.importance === '')
-      errors.importance = "Importance can't be empty";
-
-  if(data.description === '')
-      errors.description = "Description can't be empty";
-
-  const isValid = Object.keys(errors).length === 0; 
-  if(isValid){
-      const { title, importance, description } = data;
-      //Save to db here...
-      //Then successful response:
-      res.json({ category: { title, importance, description } });
-  } else {
-      res.status(404).json({ errors });
-  }
-});
-
-  
-router.put('/api/articles/updateArticle/:id', function(req, res){
-  
-    const id = req.params.id;
-    const data = req.body;
-  
-    let errors = {};
+    router.get('/api/articles/fetchCategories', function(req, res, next) {
     
-    if(data.title === '')
-        errors.title = "Title can't be empty";
+        db.collection('categories')
+          .find({})
+          .toArray((err, categories) => {
+                
+                res.json({ categories });
+        }); 
+    });
   
-    if(data.abstract === '')
-        errors.abstract = "Abstract can't be empty";
+    router.get('/api/articles/fetchArticles/:id', function(req, res, next) {
+    
+        db.collection('articles')
+          .find({ categoryId : new mongodb.ObjectId(req.params.id)})
+          .toArray((err, articles) => {
+
+               res.json({ articles });
+        }); 
+    });
   
-    if(data.body === '')
-        errors.body = "Body can't be empty";
+    router.get('/api/articles/fetchArticle/:id', function(req, res, next) {
+    
+        db.collection('articles')
+          .findOne({ _id : new mongodb.ObjectId(req.params.id)})
+          .toArray((err, article) => {
+              
+               res.json({ article });
+        });
+    });
   
-    if(data.city === '')
-        errors.city = "City can't be empty";
+    router.post('/api/articles/saveComment', function(req, res){
   
-    if(data.country === '')
-        errors.country = "Country can't be empty";
+        const data = req.body;
+    
+        let errors = {};
+        
+        if(data.name === '')
+            errors.name = "Name can't be empty";
+        if(data.email === '')
+            errors.email = "Email can't be empty";
+        if(data.body === '')
+            errors.body = "Body can't be empty";
+    
+        const isValid = Object.keys(errors).length === 0; 
+        if(isValid) {
+
+            const { name, email, body, articleId } = req.body;
+            
+            db.collection('comments').insert({ name, email, body, articleId }, (err, result) => {
+
+                if (err) {
+                    res.status(404).json({ errors : { global: "Something went wrong" }});
+                }
+                else {
+                    res.json({ comments : result.ops[0] });
+                }
+            });
+
+        } else {
+            res.status(404).json({ errors });
+        }
+    });
   
-    const isValid = Object.keys(errors).length === 0; 
-    if(isValid){
-        const { title, importance, description } = req.body;
-        //Save to db here...
-        //Then successful response:
-        res.json({ article : req.body });
-    } else {
-        res.status(404).json({ errors });
-    }
-});
-  
-  router.post('/api/articles/saveArticle', function(req, res){
+    router.delete('/api/articles/deleteCategory/:id', function(req, res) {
+        
+        db.collection('categories').deleteOne({ _id: new mongodb.ObjectId(req.params.id)}, (err, r) =>{
+            if(err){
+                res.status(500).json({ errors: { global : err }});
+                return;
+            }
+        });
+
+        res.json({});  
+    });
+    
+    router.delete('/api/articles/deleteArticle/:id', function(req, res) {
+        
+        db.collection('articles').deleteOne({ _id: new mongodb.ObjectId(req.params.id)}, (err, r) =>{
+            if(err){
+                res.status(500).json({ errors: { global : err }});
+                return;
+            }
+        });
+
+        res.json({});    
+    });
+        
+    router.put('/api/articles/updateCategory/:id', function(req, res){
+         
+        const data = req.body;
+    
+        let errors = {};
+        
+        if(data.title === '')
+            errors.title = "Title can't be empty";
+    
+        if(data.importance === '')
+            errors.importance = "Importance can't be empty";
+    
+        if(data.description === '')
+            errors.description = "Description can't be empty";
+    
+        const isValid = Object.keys(errors).length === 0; 
+        if(isValid){
+            const { title, importance, description } = data;            
+            db.collection('categories').findOneAndUpdate(
+                { _id: new mongodb.ObjectId(req.params.id) },
+                { $set: { title, importance, description } },
+                { returnOriginal: false },
+                (err, result) => {
+                    if(err) {
+                        res.status(500).json({ errors: { global: err }});
+                        return; 
+                    }
+                    res.json({ category: result.value });
+                } 
+            ); 
+            
+        } else {
+            res.status(404).json({ errors });
+        }
+    });
+    
+    router.post('/api/articles/saveCategory', function(req, res){
+        
+        const data = req.body;
+        let errors = {};
+        
+        if(data.title === '')
+            errors.title = "Title can't be empty";
+    
+        if(data.importance === '')
+            errors.importance = "Importance can't be empty";
+    
+        if(data.description === '')
+            errors.description = "Description can't be empty";
+    
+        const isValid = Object.keys(errors).length === 0; 
+        if(isValid) {
+            
+            const { title, importance, description } = data;
+
+            db.collection('categories').insert({ title, importance, description }, (err, result) => {
+                if(err) {
+                    res.status(500).json({ errors: { global: "Something went wrong" }});
+                } else {
+                    res.json({ category: result.ops[0] });
+                }
+            });
+
+        } else {
+            res.status(404).json({ errors });
+        }
+    });
+    
+        
+    router.put('/api/articles/updateArticle/:id', function(req, res){
+         
+        const data = req.body;
+        
+        let errors = {};
+        
+        if(data.title === '')
+            errors.title = "Title can't be empty";
+        
+        if(data.abstract === '')
+            errors.abstract = "Abstract can't be empty";
+        
+        if(data.body === '')
+            errors.body = "Body can't be empty";
+        
+        if(data.city === '')
+            errors.city = "City can't be empty";
+        
+        if(data.country === '')
+            errors.country = "Country can't be empty";
+        
+        const isValid = Object.keys(errors).length === 0; 
+        if(isValid){
+            const { title, abstract, body, city, country } = req.body;
+            db.collection('articles').findOneAndUpdate(
+                { _id: new mongodb.ObjectId(req.params.id) },
+                { $set: { title, abstract, body, city, country } },
+                { returnOriginal: false },
+                (err, result) => {
+                    if(err) {
+                        res.status(500).json({ errors: { global: err }});
+                        return; 
+                    }
+                    res.json({ article: result.value });
+                } 
+            ); 
+
+        } else {
+            res.status(404).json({ errors });
+        }
+    });
+    
+    router.post('/api/articles/saveArticle', function(req, res){
+        
+        const data = req.body;
+        let errors = {};
       
-    const data = req.body;
-    let errors = {};
+        if(data.title === '')
+            errors.title = "Title can't be empty";
     
-    if(data.title === '')
-        errors.title = "Title can't be empty";
+        if(data.abstract === '')
+            errors.abstract = "Abstract can't be empty";
+    
+        if(data.body === '')
+            errors.body = "Body can't be empty";
+    
+        if(data.city === '')
+            errors.city = "City can't be empty";
+    
+        if(data.country === '')
+            errors.country = "Country can't be empty";
+    
+        const isValid = Object.keys(errors).length === 0; 
+        if(isValid){
+            const { title, abstract, body, city, country } = data;
+            db.collection('articles').insert({ title, abstract, body, city, country }, (err, result) => {
+                if(err) {
+                    res.status(500).json({ errors: { global: "Something went wrong" }});
+                } else {
+                    res.json({ article: result.ops[0] });
+                }
+            });
+        } else {
+            res.json({ article : result.ops[0] });
+      }
+    });
   
-    if(data.abstract === '')
-        errors.abstract = "Abstract can't be empty";
-  
-    if(data.body === '')
-        errors.body = "Body can't be empty";
-  
-    if(data.city === '')
-        errors.city = "City can't be empty";
-  
-    if(data.country === '')
-        errors.country = "Country can't be empty";
-  
-    const isValid = Object.keys(errors).length === 0; 
-    if(isValid){
-        const { title, abstract, body, city, country } = data;
-        //Save to db here...
-        //Then successful response:
-        res.json({ article: req.body });
-    } else {
-        res.status(404).json({ errors });
-    }
-});
+    router.get('/api/articles/fetchCategory/:id', function(req, res, next) {
+        
+        db.collection('categories')
+          .findOne({ _id : new mongodb.ObjectId(req.params.id)})
+          .toArray((err, category) => {
+              
+               res.json({ category });
+        });
+    });
+})
 
-router.get('/api/articles/fetchCategory/:id', function(req, res, next) {
-  
-  const testData = { id: 1, title: "ahahah", description: "asdfffffffffffgbczmx xzcxc hjdksfsd sd", importance: 4 };     
-  res.json(testData);
-});
 
 module.exports = router;
